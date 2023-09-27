6cb4ae0c4c6519b3ab6585a8850aed9f3f179ab8
Arnau-webDev <arnauma16@gmail.com>
2023-09-27 17:39:46 +0200

    Mui TextField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import MuiButton from './components/MuiButton';
 import MuiTypography from './components/MuiTypography';
+import { MuiTextField } from './components/MuiTextField';
 
 function App() {
   
 	return (
 		<>
 			{/* <MuiTypography /> */}
-			<MuiButton />
+			{/* <MuiButton /> */}
+			<MuiTextField />
 		</>
 	);
 }
--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -25,54 +25,30 @@ const MuiButton = () => {
 	return (
 		<Stack spacing={4}>
 			<Stack spacing={2} direction='row'>
-				<Button variant='text' href='https://google.com'>
-          Text
-				</Button>
+				<Button variant='text' href='https://google.com'>Text</Button>
 				<Button variant='contained'>Contained</Button>
 				<Button variant='outlined'>Outlined</Button>
 			</Stack>
 			<Stack spacing={2} direction='row'>
-				<Button variant='contained' color='primary'>
-          Primary
-				</Button>
-				<Button variant='contained' color='secondary'>
-          Secondary
-				</Button>
-				<Button variant='contained' color='error'>
-          Error
-				</Button>
-				<Button variant='contained' color='warning'>
-          Warning
-				</Button>
-				<Button variant='contained' color='info'>
-          Info
-				</Button>
-				<Button variant='contained' color='success'>
-          Success
-				</Button>
+				<Button variant='contained' color='primary'>Primary</Button>
+				<Button variant='contained' color='secondary'>Secondary</Button>
+				<Button variant='contained' color='error'>Error</Button>
+				<Button variant='contained' color='warning'>Warning</Button>
+				<Button variant='contained' color='info'>Info</Button>
+				<Button variant='contained' color='success'>Success</Button>
 			</Stack>
 			<Stack display='block' spacing={2} direction='row'>
-				<Button variant='contained' size='small'>
-          Small
-				</Button>
-				<Button variant='contained' size='medium'>
-          Medium
-				</Button>
-				<Button variant='contained' size='large'>
-          Large
-				</Button>
+				<Button variant='contained' size='small'>Small</Button>
+				<Button variant='contained' size='medium'>Medium</Button>
+				<Button variant='contained' size='large'>Large</Button>
 			</Stack>
 			<Stack spacing={2} direction='row'>
 				<Button
 					variant='contained'
 					startIcon={<SendIcon />}
 					disableRipple
-					onClick={() => alert('Clicked')}>
-          Send
-				</Button>
-				<Button variant='contained' endIcon={<SendIcon />} disableElevation>
-          Send
-				</Button>
+					onClick={() => alert('Clicked')}>Send</Button>
+				<Button variant='contained' endIcon={<SendIcon />} disableElevation>Send</Button>
 				<IconButton color='success' size='small' aria-label='send'>
 					<SendIcon />
 				</IconButton>
