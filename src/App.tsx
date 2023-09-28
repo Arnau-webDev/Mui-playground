b2779e77d183ca5291e59bf4a3e695c2019be938
Arnau-webDev <arnauma16@gmail.com>
2023-09-28 18:11:16 +0200

    Mui ImageList component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import MuiAutocomplete from './components/MuiAutocomplete';
 import MuiLayout from './components/MuiLayout';
 import MuiCard from './components/MuiCard';
 import MuiAccordion from './components/MuiAccordion';
+import MuiImageList from './components/MuiImageList';
 
 function App() {
   
@@ -27,7 +28,8 @@ function App() {
 			{/* <MuiAutocomplete /> */}
 			{/* <MuiLayout /> */}
 			{/* <MuiCard /> */}
-			<MuiAccordion />
+			{/* <MuiAccordion /> */}
+			<MuiImageList />
 		</>
 	);
 }
